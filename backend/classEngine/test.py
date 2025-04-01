024fc15e81a79ad9403a56089c142600ffbdba6e
rdi <info@alkemata.com>
2025-04-01 09:41:13 +0200

    debug

--- backend/classEngine/test.py
+++ backend/classEngine/test.py
@@ -16,6 +16,7 @@ ELASTICSEARCH_HOSTS = "http://elasticsearch:9200"
 ELASTICSEARCH_USER = os.environ.get("ELASTICSEARCH_USER")
 print("username: "+ELASTICSEARCH_USER)
 ELASTICSEARCH_PASSWORD = os.environ.get("ELASTICSEARCH_PASSWORD")
+print("password : "+ELASTICSEARCH_PASSWORD)
 INDEX_NAME = "hybrid_search_index"
 SBERT_MODEL_NAME = 'all-MiniLM-L6-v2' # Or any other SBERT model
 

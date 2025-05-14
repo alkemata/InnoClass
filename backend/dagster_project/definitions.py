f5dd4bc2cb939c1abc2bf71f23e588f40b8e3e19
rdi <info@alkemata.com>
2025-05-14 14:06:36 +0200

    added definitions

---
+++ backend/dagster_project/definitions.py
@@ -0,0 +1,12 @@
+from dagster import Definitions
+#from my_dagster_project.assets import my_asset_group1, my_asset_group2
+from example_job import hello_job
+#from my_dagster_project.resources.my_resources import my_resource_defs
+
+defs = Definitions(
+    assets=[],
+    jobs=[hello_job],
+    resources=[],
+    schedules=[],
+    sensors=[],
+)

8f19fd3719b3ebea315826a2ddd83bf3506ce9f7
rdi <info@alkemata.com>
2025-05-14 22:44:01 +0200

    debug

--- backend/dagster_project/assets.py
+++ backend/dagster_project/assets.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import os
 import gzip
+import re
+import xml.etree.ElementTree as ET
+import spacy
+from typing import List, Optional
 from dagster import (
     asset, AssetExecutionContext,
     Output, MetadataValue
 )
 from dagster import asset_check, AssetCheckResult, AssetCheckSeverity, Config
+
+nlp = spacy.blank('en')
+nlp.add_pipe('sentencizer')
 
 FILE_PATH = "/opt/project_data/raw_data.dat.gz"
 
@@ -30,7 +38,8 @@ class MyAssetConfig(Config):
 @asset
 def raw_file_asset(config: MyAssetConfig) -> Output[pd.DataFrame]:
     file_name = config.file_name
-    
+    contents = os.listdir("/opt/")
+    print(contents)
     # Load file
     df = load_list(file_name)
 
@@ -55,5 +64,22 @@ def text_column_not_empty(context, raw_file_asset: pd.DataFrame) -> AssetCheckRe
             severity=AssetCheckSeverity.ERROR,
             description="Some 'text' values are empty."
         )
-
     return AssetCheckResult.passed()
+
+# Keywords to search for in headings (allowing fuzzy matching with up to one error)
+keyword1 = ["scope of the invention","Description of the Related Art", "TECHNICAL SCOPE","Description of Related Art","REVEALING THE INVENTION","background of the invention", "background of the disclosure", "field of the invention", "technical field","summary","industrial applicability","field of the disclosure","background",  "prior art", "state of the art"]
+keyword2=["background","The present invention regards","herein described subject matter", "It is well known" "technology described herein", "field of the disclosure", "field of the invention", "subject of the invention", "belongs to the field", "invention is","invention relates to", "present invention refers to"]
+
+
+@asset
+def extracted_data_asset(config: MyAssetConfig) -> Output[pd.DataFrame]:
+    file_name = config.file_name
+    
+    # Load file
+    df = load_list(file_name)
+
+    # Attach metadata: number of lines
+    metadata = {
+        "num_rows": MetadataValue.int(len(df)),
+        "file_name": MetadataValue.text(file_name)
+    }

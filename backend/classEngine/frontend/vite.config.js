e8d495385593e4c4442c17253abfd07e105d9fc4
rdi <info@alkemata.com>
2025-05-18 13:30:04 +0200

    correct

--- backend/classEngine/frontend/vite.config.js
+++ backend/classEngine/frontend/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 9000,
     proxy: {
       '/api': 'http://0.0.0.0:9000'
-  }
+  },
+    allowedHosts: ['innoclass.alkemata.com'],
+    host: true // allow external connections (e.g., for LAN/public access)
 }})
 

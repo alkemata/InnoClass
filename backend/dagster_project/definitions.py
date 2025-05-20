742da770ec65a28bea894fee62b1f1fa0de7c7ef
rdi <info@alkemata.com>
2025-05-20 12:02:58 +0200

    correct

--- backend/dagster_project/definitions.py
+++ backend/dagster_project/definitions.py
@@ -1,12 +1,12 @@
-from dagster import Definitions, load_assets_from_current_module,ResourceDefinition
+from dagster import Definitions, load_assets_from_module,ResourceDefinition
 
 from sensors import file_update_sensor
-from assets import text_column_not_empty
+import assets
 from resources import SBERT, qdrant,es
 #from example_job import hello_job
 #from my_dagster_project.resources.my_resources import my_resource_defs
 
-all_assets=load_assets_from_current_module()
+all_assets=load_assets_from__module([assets])
 print(all_assets)
 
 defs = Definitions(

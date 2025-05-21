ec977db207be2ec3de2907e0c75672b1f2d35a7a
rdi <info@alkemata.com>
2025-05-21 16:01:43 +0200

    work on fastapii

--- backend/classEngine/entrypoint.sh
+++ backend/classEngine/entrypoint.sh
@@ -3,8 +3,8 @@
 # [ "$BUILD_ENV" = "server" ]; then
     echo "Running in server mode"
     # Your special command here
-#    exec gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 api-server:app --log-level debug
-    npm run dev
+    exec gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 api-server:app --log-level debug
+#    npm run dev
 #else
 #    echo "Running python command"
     # Your default command here
--- backend/classEngine/frontend/src/api.ts
+++ backend/classEngine/frontend/src/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://api.innoclass.alkemata.com:8000",
+  baseURL: "https://api.innoclass.alkemata.com:8000",
 });

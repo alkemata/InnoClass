8b90f45edbe0df847b9dd92d51266df7835dafd5
rdi <info@alkemata.com>
2025-05-21 20:49:29 +0200

    correct

--- backend/dagster_project/assets.py
+++ backend/dagster_project/assets.py
@@ -248,7 +248,7 @@ def check_qdrant_health(context: AssetExecutionContext):
 
 # 4. Asset: Run threshold search for queries and persist scores
 # ------------------
-@asset(deps=["index_texts", "targets_asset", "goals_asset"],required_resource_keys={"es_resource","model","qdrant_resource"})
+@asset(deps=["es_patent_light","index_texts", "targets_asset", "goals_asset"],required_resource_keys={"es_resource","model","qdrant_resource"})
 def search_and_store(context: AssetExecutionContext, config: MyAssetConfig, goals_asset) -> None:
     """
     Encode a list of queries, run range searches in Qdrant,

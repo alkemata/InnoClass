3e2fe8d8d1a7a910d4fc646ed9a9b8a78588251b
rdi <info@alkemata.com>
2025-05-21 16:34:12 +0200

    correct

--- backend/classEngine/entrypoint.sh
+++ backend/classEngine/entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# [ "$BUILD_ENV" = "server" ]; then
-    echo "Running in server mode"
-    # Your special command here
-    exec gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 api-server:app --log-level debug
-#    npm run dev
-#else
-#    echo "Running python command"
-    # Your default command here
-#    exec python -u embedsearch.py
-#fi
+exec gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 api-server:app --log-level debug

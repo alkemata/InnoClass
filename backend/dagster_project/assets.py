b858e83a148ed1e3080a06879e144da27472f7ef
rdi <info@alkemata.com>
2025-05-25 22:20:54 +0200

    correct

--- backend/dagster_project/assets.py
+++ backend/dagster_project/assets.py
@@ -89,7 +89,7 @@ def extracted_data_asset(raw_file_asset, config: MyAssetConfig) -> Output[List[d
     tracker.start()
     try:
         extracted = fu.process_texts(raw_file_asset, fu.keyword1, fu.keyword2)
-        for item in extracted[:1]:
+        for item in extracted:
             print(item['sdg'])
     finally:
         emissions_data = tracker.stop()

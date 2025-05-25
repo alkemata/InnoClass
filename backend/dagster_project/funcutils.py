07b89da5567f90a4724bbd9acebafa73ead4cf0e
rdi <info@alkemata.com>
2025-05-25 19:57:50 +0200

    debug

--- backend/dagster_project/funcutils.py
+++ backend/dagster_project/funcutils.py
@@ -227,7 +227,10 @@ def extract_text_simple(
 
 def process_texts(texts, keyword1, keyword2, min_sentence_length=5): # min_sentence_length is now mostly handled in extract_text_simple
     results = []
+    nb1=0
     for item in texts:
+        print(nb1)
+        nb1=nb1+1
         # Note: extract_text_simple now returns a list of strings (sentences)
         result = extract_text_simple(item["original_text"], keyword1, keyword2, keyword2)
         item["cleaned_text"]=result

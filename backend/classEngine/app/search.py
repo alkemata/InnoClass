9e250355b4ca716d0e7e0da3047002f3f4e4b668
rdi <info@alkemata.com>
2025-05-26 21:32:14 +0200

    correct

--- backend/classEngine/app/search.py
+++ backend/classEngine/app/search.py
@@ -84,11 +84,12 @@ async def search(req: SearchRequest):
         hits.append(Hit(
             id=h["_id"],
             title=src["title"],
-            cleaned_text=src.get("cleaned_text",""),
+            cleaned_text=src.get("cleaned_text","")[:100],
             sdgs=[SdgItem(**sdg) for sdg in src.get("sdg",[])], # Convert to SdgItem objects
             targets=src.get("target",[]),
             up=src.get("up",0),
             down=src.get("down",0)
         ))
+        print(sdgs)
     print(str(len(hits)))
     return SearchResponse(hits=hits, total=res["hits"]["total"]["value"])
